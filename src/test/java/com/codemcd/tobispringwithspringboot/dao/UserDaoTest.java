2da9a9ba3b4b5866a16f8959ef3b74f38659972a
CODEMCD <psbum1609@gmail.com>
2020-09-07 23:52:19 +0900

    리스트 1-12 관계설정 책임이 추가된 UserDao 클라이언트인 main() 메소드

---
+++ src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
@@ -0,0 +1,11 @@
+package com.codemcd.tobispringwithspringboot.dao;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+
+        UserDao userDao = new UserDao(connectionMaker);
+    }
+}
