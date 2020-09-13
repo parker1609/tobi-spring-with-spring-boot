9f243947dcffc5f90ffeb43d76334ccc69e8e0ee
CODEMCD <psbum1609@gmail.com>
2020-09-13 22:24:54 +0900

    리스트 1-17 생성 오브젝트 코드 수정

--- src/main/java/com/codemcd/tobispringwithspringboot/dao/DaoFactory.java
+++ src/main/java/com/codemcd/tobispringwithspringboot/dao/DaoFactory.java
@@ -6,14 +6,18 @@ import com.codemcd.tobispringwithspringboot.dao.user.UserDao;
 
 public class DaoFactory {
     public UserDao userDao() {
-        return new UserDao(new DConnectionMaker());
+        return new UserDao(getConnectionMaker());
     }
 
     public AccountDao accountDao() {
-        return new AccountDao(new DConnectionMaker());
+        return new AccountDao(getConnectionMaker());
     }
 
     public MessageDao messageDao() {
-        return new MessageDao(new DConnectionMaker());
+        return new MessageDao(getConnectionMaker());
+    }
+
+    private ConnectionMaker getConnectionMaker() {
+        return new DConnectionMaker();
     }
 }
