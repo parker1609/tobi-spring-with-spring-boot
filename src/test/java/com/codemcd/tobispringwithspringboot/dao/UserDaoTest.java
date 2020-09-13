b5159573d13ada9b23169796be37d5e58e76a2c9
CODEMCD <psbum1609@gmail.com>
2020-09-13 22:09:36 +0900

    리스트 1-15 팩토리를 사용하도록 수정한 UserDaoTest

--- src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
+++ src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
@@ -4,8 +4,6 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new DaoFactory().userDao();
     }
 }
