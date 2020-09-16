917036a19eb2f483e14534bad7c64e5f32b013a9
CODEMCD <psbum1609@gmail.com>
2020-09-16 23:05:07 +0900

    리스트 1-19 애플리케이션 컨텍스트를 적용한 UserDaoTest

--- src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
+++ src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
@@ -1,11 +1,13 @@
 package com.codemcd.tobispringwithspringboot.dao;
 
 import com.codemcd.tobispringwithspringboot.dao.user.UserDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao userDao = new DaoFactory().userDao();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
     }
 }
