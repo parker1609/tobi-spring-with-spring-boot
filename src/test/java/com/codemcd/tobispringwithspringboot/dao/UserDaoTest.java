cbde992c883162df5ac83a5a22d87dd42f2ea677
CODEMCD <psbum1609@gmail.com>
2020-09-21 19:19:19 +0900

    리스트 1-20 직접 생성한 Dao Factory 오브젝트 출력 코드

--- src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
+++ src/test/java/com/codemcd/tobispringwithspringboot/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package com.codemcd.tobispringwithspringboot.dao;
 
 import com.codemcd.tobispringwithspringboot.dao.user.UserDao;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
@@ -9,5 +10,15 @@ public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
+    }
+
+    @Test
+    void list_1_20() {
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao1 = daoFactory.userDao();
+        UserDao userDao2 = daoFactory.userDao();
+
+        System.out.println(userDao1);
+        System.out.println(userDao2);
     }
 }
