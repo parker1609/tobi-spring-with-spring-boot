742a76e35fff0f1c75eb52619d65da8b0596c0fc
parker1609 <psbum1609@gmail.com>
2021-04-04 19:42:01 +0900

    리스트 2-27 (p203) 스프링 테스트 컨텍스트에 대한 학습 테스트

--- src/test/java/com/codemcd/tobispringwithspringboot/learningtest/junit/JUintTest.java
+++ src/test/java/com/codemcd/tobispringwithspringboot/learningtest/junit/JUintTest.java
@@ -2,28 +2,51 @@ package com.codemcd.tobispringwithspringboot.learningtest.junit;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 public class JUintTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUintTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     void test1() {
-        Assertions.assertThat(testObjects).doesNotContain(this);
+        assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     void test2() {
-        Assertions.assertThat(testObjects).doesNotContain(this);
+        assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     void test3() {
-        Assertions.assertThat(testObjects).doesNotContain(this);
+        assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 }
