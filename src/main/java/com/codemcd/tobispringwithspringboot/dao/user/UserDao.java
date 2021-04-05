91b64f95e707c6e6904b3c981197aeed008fd3c6
parker1609 <psbum1609@gmail.com>
2021-04-05 11:06:19 +0900

    리스트 3-16 (p227) add() 메서드 내의 로컬 클래스로 이전한 AddStatement

--- src/main/java/com/codemcd/tobispringwithspringboot/dao/user/UserDao.java
+++ src/main/java/com/codemcd/tobispringwithspringboot/dao/user/UserDao.java
@@ -17,6 +17,25 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
+        class AddStatement implements StatementStrategy {
+            User user;
+
+            public AddStatement(User user) {
+                this.user = user;
+            }
+
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(
+                        "INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        }
+
         StatementStrategy st = new AddStatement(user);
         jdbcContextWithStatementStrategy(st);
     }
