078b35abc411b1a373e3c8a8e50cab68d8f69cda
parker1609 <psbum1609@gmail.com>
2021-04-05 11:13:38 +0900

    익명 내부 클래스 -> 람다식으로 변경

--- src/main/java/com/codemcd/tobispringwithspringboot/dao/user/UserDao.java
+++ src/main/java/com/codemcd/tobispringwithspringboot/dao/user/UserDao.java
@@ -17,17 +17,14 @@ public class UserDao {
     }
 
     public void add(final User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement(
-                        "INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
+        jdbcContextWithStatementStrategy(c -> {
+            PreparedStatement ps = c.prepareStatement(
+                    "INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-                return ps;
-            }
+            return ps;
         });
     }
 
