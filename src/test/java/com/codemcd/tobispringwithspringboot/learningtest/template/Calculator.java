0fb308efe42719b0651ec0e7cdf266681b6c0d0a
parker1609 <psbum1609@gmail.com>
2021-04-05 14:09:59 +0900

    리스트 ~ 3-42 (p258) 타입 파라미터를 추가해서 제네릭 메서드로 만든 lineReadTemplate()

--- src/test/java/com/codemcd/tobispringwithspringboot/learningtest/template/Calculator.java
+++ src/test/java/com/codemcd/tobispringwithspringboot/learningtest/template/Calculator.java
@@ -6,21 +6,21 @@ import java.io.IOException;
 
 public class Calculator {
 
-    public int calcSum(String filepath) throws IOException {
-        LineCallback sumCallback = (line, value) -> value + Integer.parseInt(line);
+    public Integer calcSum(String filepath) throws IOException {
+        LineCallback<Integer> sumCallback = (line, value) -> value + Integer.parseInt(line);
 
         return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        LineCallback sumCallback = (line, value) -> value * Integer.parseInt(line);
+        LineCallback<Integer> multiplyCallback = (line, value) -> value * Integer.parseInt(line);
 
-        return lineReadTemplate(filepath, sumCallback, 1);
+        return lineReadTemplate(filepath, multiplyCallback, 1);
     }
 
-    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
-            Integer res = initVal;
+            T res = initVal;
             String line = null;
             while ((line = br.readLine()) != null) {
                 res = callback.doSomethingWithLine(line, res);
--- src/test/java/com/codemcd/tobispringwithspringboot/learningtest/template/LineCallback.java
+++ src/test/java/com/codemcd/tobispringwithspringboot/learningtest/template/LineCallback.java
@@ -1,6 +1,6 @@
 package com.codemcd.tobispringwithspringboot.learningtest.template;
 
 @FunctionalInterface
-public interface LineCallback {
-    Integer doSomethingWithLine(String line, Integer value);
+public interface LineCallback<T> {
+    T doSomethingWithLine(String line, T value);
 }
