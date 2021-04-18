07ec54e3f521e32b6522b1a49657948b16b46f81
parker1609 <psbum1609@gmail.com>
2021-04-18 10:03:28 +0900

    리스트 5-18 (p332) 사용자 레벨 업그레이드 메서드

--- src/main/java/com/codemcd/tobispringwithspringboot/user/service/UserService.java
+++ src/main/java/com/codemcd/tobispringwithspringboot/user/service/UserService.java
@@ -1,11 +1,37 @@
 package com.codemcd.tobispringwithspringboot.user.service;
 
 import com.codemcd.tobispringwithspringboot.user.dao.UserDao;
+import com.codemcd.tobispringwithspringboot.user.domain.Level;
+import com.codemcd.tobispringwithspringboot.user.domain.User;
+
+import java.util.List;
 
 public class UserService {
     private UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public void upgradeLevels() {
+        List<User> users = userDao.getAll();
+        for (User user: users) {
+            Boolean changed = null;
+            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+                user.setLevel(Level.SILVER);
+                changed = true;
+            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+                user.setLevel(Level.GOLD);
+                changed = true;
+            } else if (user.getLevel() == Level.GOLD) {
+                changed = false;
+            } else {
+                changed = false;
+            }
+
+            if (changed) {
+                userDao.update(user);
+            }
+        }
     }
 }
