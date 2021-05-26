e20b422e16b7862a04fb9130887e609866596cd9
parker1609 <psbum1609@gmail.com>
2021-05-26 10:14:37 +0900

    리스트 5-52 (p385) 스프링의 MailSender를 이용한 메일 발송 메서드

--- src/main/java/com/codemcd/tobispringwithspringboot/user/service/UserService.java
+++ src/main/java/com/codemcd/tobispringwithspringboot/user/service/UserService.java
@@ -4,19 +4,13 @@ import com.codemcd.tobispringwithspringboot.user.dao.UserDao;
 import com.codemcd.tobispringwithspringboot.user.domain.Level;
 import com.codemcd.tobispringwithspringboot.user.domain.User;
 import lombok.Getter;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.util.List;
-import java.util.Properties;
 
 @Getter
 public class UserService {
@@ -72,21 +66,16 @@ public class UserService {
     }
 
     private void sendUpgradeEMail(User user) {
-        Properties props = new Properties();
-        props.put("mail.stmp.host", "mail.kug.org");
-        Session s = Session.getInstance(props, null);
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("mail.server.com");
 
-        MimeMessage message = new MimeMessage(s);
-        try {
-            message.setFrom(new InternetAddress("useradmin@ksug.org"));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
-            message.setSubject("Upgrade 안내");
-            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("useradmin@ksug.org");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
 
-            Transport.send(message);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+        mailSender.send(mailMessage);
     }
 
     public void add(User user) {
